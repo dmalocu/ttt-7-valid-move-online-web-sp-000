3007f74d19141b8832ab19177d2005bb960fdf78
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-09 15:31:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,35 @@
-# code your #valid_move? method here
+#valid move , non empty
+#not valid , empty 
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+
+#valid move , empty 
+#not valid , non empty
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def valid_move?(board, index)
+  if empty_board?(board, index) && user_input.between?(0,8)
+    return true 
+  elsif non_empty_board?(board, index) && user_input.between?(0,8)
+    return true
+  elsif !(user_input.between?(0,8))
+    return false 
+end
+
+def empty_board?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return true
+  else 
+    return false 
+  end
+end
+
+def non_empty_board?(board, index)
+  if !(board[index] == "" || board[index] == " " || board[index] == nil)
+    return true 
+  else 
+    return false 
+  end
+end
