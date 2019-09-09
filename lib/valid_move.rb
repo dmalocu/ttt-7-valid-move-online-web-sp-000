839d3edf969bc69df0385e005ce76d19d4aa94dd
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-09 15:37:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
       return false
     else
       return true
