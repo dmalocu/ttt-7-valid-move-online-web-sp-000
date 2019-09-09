d336f395ab3f188e06e3301b9481b00032e4c7bd
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-09 15:38:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def valid_move?(board, index)
     end
   end
 
-  def on_board?(num)
+  def valid_num?(num)
     if num.between?(0, 8) == true
       return true
     else
@@ -15,7 +15,7 @@ def valid_move?(board, index)
     end
   end
 
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+  if (position_taken?(board, index)) == false && (valid_num?(index) == true)
     return true
   else
     return false
